9c60cd211753834a4120a10d1cb4407db235dedd
bdCalling <promotionbd321@gmail.com]>
2024-09-18 15:04:43 -0700

    live location tracking

--- lib/pages/CurrentLocationMap.dart
+++ lib/pages/CurrentLocationMap.dart
@@ -1,47 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_map_by_masum/controller/maps_controller.dart';
+import 'package:google_map_by_masum/pages/live_location.dart';
 import 'package:google_map_by_masum/pages/polyline_screen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class CurrentLocationMap extends StatelessWidget {
-   CurrentLocationMap({super.key});
+import 'MyCurrentLatitudeAndLongitude.dart';
 
+class CurrentLocationMap extends StatelessWidget {
+  CurrentLocationMap({super.key});
 
   final MapsController mapsController = Get.put(MapsController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Current Location '),centerTitle: true,),
-      body: Obx(() => GoogleMap(
-        onMapCreated:mapsController.onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: mapsController.currentPosition.value,
-          zoom: 10.0,
+        appBar: AppBar(
+          title: const Text('Current Location '),
+          centerTitle: true,
         ),
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        cameraTargetBounds: CameraTargetBounds.unbounded,
-        markers: mapsController.currentMarkerLocation.value != null
-            ? {mapsController.currentMarkerLocation.value!}
-            : {},
-      )),
+        body: Obx(() => Column(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: GoogleMap(
+                    onMapCreated: mapsController.onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: mapsController.currentPosition.value,
+                      zoom: 10.0,
+                    ),
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: true,
+                    cameraTargetBounds: CameraTargetBounds.unbounded,
+                    markers: mapsController.currentMarkerLocation.value != null
+                        ? {mapsController.currentMarkerLocation.value!}
+                        : {},
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                    child: TextButton(
+                        onPressed: () {
+                          Get.to(MyCurrentLatitudeAndLongitude());
+                        },
+                        child: const Text(
+                          'Location Listener',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 25),
+                        ))),
 
-      ///================================PolyLine Screen=================
-      floatingActionButton:
-      TextButton(
-        onPressed: () {
-          Get.to( PolylineScreen());
-        },
-        style: TextButton.styleFrom(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.blue, // Text color
-        ),
-        child: const Text('PolyLine Screen'),
-      )
+                Expanded(
+                  flex: 2,
+                    child: TextButton(
+                        onPressed: () {
+                          Get.to(LiveLocation());
+                        },
+                        child: const Text(
+                          'Live Location ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 25),
+                        ))),
+              ],
+            )),
 
-    );
+        ///================================PolyLine Screen=================
+        floatingActionButton: TextButton(
+          onPressed: () {
+            Get.to(PolylineScreen());
+          },
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue, // Text color
+          ),
+          child: const Text('PolyLine Screen'),
+        ));
   }
-
 }
---
+++ lib/pages/live_location.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_map_by_masum/controller/live_location_controller.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LiveLocation extends StatelessWidget {
+  final LiveLocationController locationController = Get.put(LiveLocationController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Live Location on Google Map'),
+      ),
+      body: Obx(() {
+        // Show Google Map and update it with current location
+        return GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: locationController.currentLocation.value,
+            zoom: 14.0,
+          ),
+          markers: {
+            Marker(
+              markerId: MarkerId('currentLocation'),
+              position: locationController.currentLocation.value,
+              infoWindow: InfoWindow(title: 'You are here'),
+            ),
+          },
+          onMapCreated: (GoogleMapController controller) {
+            // Move the camera to the current location when map is created
+            controller.animateCamera(
+              CameraUpdate.newLatLng(locationController.currentLocation.value),
+            );
+          },
+        );
+      }),
+    );
+  }
+}
